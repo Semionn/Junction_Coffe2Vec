d02dc97eb98d27d831aab73f3720f11b6c08469c
Semyon Polyakov <spolyakov@eva618000.vkontakte.ru>
2020-11-07 13:37:39 +0300

    more pages

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 import json
+import logging
 
 app = Flask(__name__)
 
@@ -21,11 +22,22 @@ def create_plot():
     ]
     graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
+#
+# @app.route('/')
+# def index():
+#     bar = create_plot()
+#     return render_template('index.html', plot=bar)
+
+# @app.route('/')
+# def index():
+#     bar = create_plot()
+#     return render_template('index_figma_test.html', plot=bar)
 
 @app.route('/')
 def index():
+    logging.warning(plotly.__version__)
     bar = create_plot()
-    return render_template('index.html', plot=bar)
+    return render_template('index_figma2_page.html', plot=bar)
 
 
 if __name__ == '__main__':
